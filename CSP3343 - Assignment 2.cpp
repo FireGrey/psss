ee5f3bfeb9caf9ff04bf74e8d7ac15f4e6adc930
Alex Till <aptill@our.ecu.edu.au>
2016-05-15 12:43:47 +0800

    Super Simple generation of misuse case candidates

--- CSP3343 - Assignment 2.cpp
+++ CSP3343 - Assignment 2.cpp
@@ -11,6 +11,16 @@
 using namespace tinyxml2;
 using namespace std;
 
+
+void createMisuseCase(vector<vector<string>> &misuseCaseList, string actor, string node, string type) {
+    vector<string> misuseTemp;
+    misuseTemp.push_back(actor);
+    misuseTemp.push_back(node);
+    misuseTemp.push_back(type);
+    misuseCaseList.push_back(misuseTemp);
+}
+
+
 int _tmain(int argc, _TCHAR* argv[])
 {
     XMLDocument useCaseDiagram;
@@ -18,9 +28,18 @@ int _tmain(int argc, _TCHAR* argv[])
 
     const char* diagramTitle = NULL;
     int nodeCount = 0;
-    vector<string> nodeList;
+    // Node List
+    // [i][0] == nodeName
+    // [i][1] == nodeType (eg. Actor)
+    vector<vector<string>> nodeList;
     int relationshipCount = 0;
-    vector<vector<string>> relationships;
+    vector<vector<string>> relationshipList;
+    // Misuse Case List
+    // [i][0] == actor
+    // [i][1] == node
+    // [i][2] == type
+    // ('actor' threatens 'node' with 'type')
+    vector<vector<string>> misuseCaseList;
 
     if (useCaseDiagram.Error()) {
         printf("Cannot Parse Xml File");
@@ -29,35 +48,88 @@ int _tmain(int argc, _TCHAR* argv[])
     {
         XMLElement* diagram = useCaseDiagram.FirstChildElement("diagram");
         diagramTitle = diagram->Attribute("title");
-        cout << "Relationships in " << diagramTitle << ":\n\n";
 
+        // For each node
         for (XMLElement* currentNode = diagram->FirstChildElement("node"); currentNode != NULL; 
             currentNode = currentNode->NextSiblingElement("node")) 
         {
+            // Get node details
             string nodeName(currentNode->Attribute("id"));
-            nodeList.push_back(nodeName);
+            string nodeType(currentNode->Attribute("type"));
+            // Insert them into array
+            vector<string> nodeTemp;
+            nodeTemp.push_back(nodeName);
+            nodeTemp.push_back(nodeType);
+            nodeList.push_back(nodeTemp);
+            // For each relationship in this node
             for (XMLElement* currentRelationship = currentNode->FirstChildElement("relationship"); currentRelationship != NULL;
                 currentRelationship = currentRelationship->NextSiblingElement("relationship")) 
             {
-                string reltype(currentRelationship->Attribute("type"));
+                // Get relationship details
+                string relType(currentRelationship->Attribute("type"));
                 string relTarget(currentRelationship->Attribute("target"));
-                vector<string> relation;
-                relation.push_back(reltype);
-                relation.push_back(nodeName);
-                relation.push_back(relTarget);
-                relationships.push_back(relation);
+                // Insert them into array
+                vector<string> relationTemp;
+                relationTemp.push_back(relType);
+                relationTemp.push_back(nodeName);
+                relationTemp.push_back(relTarget);
+                relationshipList.push_back(relationTemp);
             }
             nodeCount++;
         }
     }
-    for (int relNum = 0; relNum < relationships.size(); relNum++) 
+    for (int relNum = 0; relNum < relationshipList.size(); relNum++)
     {
-        cout << relationships[relNum][0] << " Between '" << relationships[relNum][1] 
-            << "' and '" << relationships[relNum][2] << "'\n";
-        //printf("Type: %s Between %s and %s\n", relationships[relNum][0], relationships[relNum][1], relationships[relNum][2]);
+        //cout << relationshipList[relNum][0] << " Between '" << relationshipList[relNum][1] 
+        //    << "' and '" << relationshipList[relNum][2] << "'\n";
     }
+    cout << "Nodes in " << diagramTitle << ":\n\n";
+    for (int nodeNum = 0; nodeNum < nodeList.size(); nodeNum++)
+    {
+        //cout << nodeList[nodeNum][1] << ": '" << nodeList[nodeNum][0] << "'\n";
+        if (nodeList[nodeNum][1].compare("Case") != -1) {
+            //cout << nodeList[nodeNum][0] << "\n";
+
+            // Spoofing Identity
+            // Tampering
+            // Repudiation
+            // Information Disclosure
+            // Denial of Service
+            // Elevation of Privilege)
+
+            // Create Misuse Cases
+            // User - Information Disclosure
+            createMisuseCase(misuseCaseList, "User", nodeList[nodeNum][0], "Leaks information");
+
+            // External Mis-Actor - Spoofing Identity
+            createMisuseCase(misuseCaseList, "External Mis-Actor", nodeList[nodeNum][0], "Falsify Identity");
+            // External Mis-Actor - Tampering
+            createMisuseCase(misuseCaseList, "External Mis-Actor", nodeList[nodeNum][0], "Tamper with Data");
+            // External Mis-Actor - Information Disclosure
+            createMisuseCase(misuseCaseList, "External Mis-Actor", nodeList[nodeNum][0], "Leaks information");
+            // External Mis-Actor - Denial of Service
+            createMisuseCase(misuseCaseList, "External Mis-Actor", nodeList[nodeNum][0], "Denial of Service");
+
+            // Internal Mis-Actor - Tampering
+            createMisuseCase(misuseCaseList, "Internal Mis-Actor", nodeList[nodeNum][0], "Tamper with Data");
+            // Internal Mis-Actor - Repudiation
+            createMisuseCase(misuseCaseList, "Internal Mis-Actor", nodeList[nodeNum][0], "Repudiation");
+            // Internal Mis-Actor - Information Disclosure
+            createMisuseCase(misuseCaseList, "Internal Mis-Actor", nodeList[nodeNum][0], "Leaks information");
+            // Internal Mis-Actor - Elevation of Privilege
+            createMisuseCase(misuseCaseList, "Internal Mis-Actor", nodeList[nodeNum][0], "Elevate Privilege");
+        }
+    }
+
+    int misuseCaseCount = 0;
+    for (int i = 0; i < misuseCaseList.size(); i++) {
+        cout << misuseCaseList[i][0] << " threatens " << misuseCaseList[i][1] << " with " << misuseCaseList[i][2] << ".\n";
+        misuseCaseCount++;
+    }
+
+    cout << misuseCaseCount;
+
     string key;
     cin >> key;
 	return 0;
 }
-
